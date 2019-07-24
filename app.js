7dd3fd5ec1617aff28adee7d604ea6549d9703bc
emcas88 <emcas88@gmail.com>
2019-07-24 15:55:10 -0400

    Using mogoose and pagination feature

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+var Feed = require('./models/feed');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
@@ -11,69 +13,35 @@ const MONGO_DB_PORT = process.env.MONGO_DB_PORT || 27017;
 const MONGO_ROOT_USERNAME = encodeURIComponent(process.env.MONGO_ROOT_USERNAME || 'mongoadmin');
 const MONGO_ROOT_PASSWORD = encodeURIComponent(process.env.MONGO_ROOT_PASSWORD || 'secret');
 
+mongoose.connect(`mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@localhost:${MONGO_DB_PORT}/?authMechanism=DEFAULT`);
+
 app.get('/feeds', (req, res) => {
 
-    const url = `mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@localhost:${MONGO_DB_PORT}/?authMechanism=DEFAULT`;
-    const client = new MongoClient(url);
+    let page = parseInt(req.query.page || 1);
+    let limit = parseInt(req.query.limit || 100);
 
-    client.connect(err => {
-
-        if (err)
-            throw err;
-
-        let db = client.db("feeds_db");
-        
-        db.collection('names').find().toArray((err, docs) => {
-
-            if (err)
-                throw err;
-            
-            console.log(docs);
+    Feed.find({})
+        .skip(limit * (page - 1))
+        .limit(limit)
+        .exec((err, feeds) => {
+            if (err) throw err;
 
             res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({data: docs}));
-            
+-           res.end(JSON.stringify({data: feeds}));
         });
-        
-        client.close();
-    });
-
 });
 
 app.post('/feeds', (req, res) => {
 
-    let newFeed = {
-        name: req.body.name 
-    };
+    var feed = new Feed();
+    feed.name = req.body.name;
 
-    const url = `mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@localhost:27017/?authMechanism=DEFAULT`;
-    const client = new MongoClient(url);
+    feed.save(err => {
+        if (err) throw err
 
-    client.connect(err => {
-
-        if (err)
-            throw err;
-
-        let db = client.db("feeds_db");
-
-        db.collection('names').insert(newFeed);
-
-        // Wait for a second before finishing up, to ensure we have written the item to disk
-        setTimeout(() => {
-
-            // Fetch the document
-            db.collection('names').findOne({name: newFeed.name}, (err, item) => {
-                if (err)
-                    throw err;
-
-                res.setHeader('Content-Type', 'application/json');
-                res.status(201).end(JSON.stringify(item));
-
-                client.close();
-            })
-        }, 100);
-    });
+        res.setHeader('Content-Type', 'application/json');
+        res.status(201).end(JSON.stringify(feed));
+    })
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
-
